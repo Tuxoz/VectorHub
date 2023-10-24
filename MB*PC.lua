f05a57f72ec28227cc4b3f3e0e892656f704be89
VectorHub <93261055+Tuxoz@users.noreply.github.com>
2023-10-24 14:36:37 +0700

    Update MB*PC.lua

--- MB*PC.lua
+++ MB*PC.lua
@@ -1,11 +1,11 @@
 repeat wait() until game:IsLoaded();
 local id = game.PlaceId
 if id == 2753915549 or id == 4442272183 or id == 7449423635 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tuxoz/VectorHub/main/BloxFruitPC%26MOBILE"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tuxoz/VectorHub/main/b"))()
 elseif id == 6918802270 or id == 14979402479 then 
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tuxoz/VectorHub/main/Hazepiece"))()
+    loadstring(game:HttpGet(""))()
 elseif id == 13772394625 then 
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tuxoz/VectorHub/main/BladeBall"))()
+    loadstring(game:HttpGet(""))()
 else
     if _G.Mode == "English"  then
     game.Players.LocalPlayer:Kick("There are no games on the list.")
